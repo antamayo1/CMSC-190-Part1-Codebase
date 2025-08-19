fe2cbf90b549d68487e74e9e35585c7c1740af7b
antamayo1 <antamayo1@up.edu.ph>
2025-08-19 20:50:54 +0800

    initial commit

---
+++ Journal.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import skimage as ski
+import numpy as np
+from functions.functions import get_RGB_channels
+
+st.set_page_config(page_title="CMSC 190 - Part 1", layout="wide")
+
+st.title('CMSC 190 - Part 1 Journal')
+st.write('By: **Aaron John N. Tamayo**')
+st.markdown("---")
+st.subheader("Tuesday, 19 August 2025")
+st.markdown('''
+Approved reference paper titled `A Color Image Encryption Scheme Utilizing a Logistic-Sine Chaotic Map and Cellular Automata`.
+Currently reading the introduction and preliminaries of the paper but I have tried to start the methodology section
+by downloading the in the paper and applying the channel split with just the image named `jelly.tiff`.
+''')
+code_1, code_2 = st.columns(2)
+with code_1:
+  st.write("**Initial Implementation**")
+  st.code('''
+  def get_RGB_channels(image):
+    red_channel = image[:, :, 0]
+    green_channel = image[:, :, 1]
+    blue_channel = image[:, :, 2]
+    return red_channel, green_channel, blue_channel
+  ''', language='python')
+with code_2:
+  st.write("**Inverted Implementation**")
+  st.code('''
+  def get_RGB_channels(image):
+    red_channel = 255 - image[:, :, 0]
+    green_channel = 255 - image[:, :, 1]
+    blue_channel = 255 - image[:, :, 2]
+    return red_channel, green_channel, blue_channel
+  ''', language='python')
+st.markdown('''
+The initial output does not seem to have similar output as the reference paper. When I tried inverting the channels
+the output was still different but closer to the expected result. Need to investigate the statement "_The R, G, and
+B are transformed with different ranks to increase deciphering difficulty_" or is this statement about the row-wise
+and column-wise transformations before encryption?
+''')
+st.markdown('''
+**GDOCS UPDATE**:
+> Added the download links for the used images.
+''')
---
+++ pages/1 - Methodology.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import skimage as ski
+import numpy as np
+from functions.functions import get_RGB_channels
+
+st.set_page_config(page_title="CMSC 190 - Part 1", layout="wide")
+
+research_paper = "A color image encryption scheme utilizing a logistic-sine chaotic map and cellular automata"
+
+st.title(research_paper)
+st.write('By: Shiji Sun, Wenzhong Yang, Yabo Yin, Xiaodan Tian, Guanghan Li, Xiangxin Deng')
+st.markdown("---")
+
+image_path = "static/Jelly.tiff"
+image = ski.io.imread(image_path)
+
+st.header("1. Channel Split")
+st.code('''
+def get_RGB_channels(image):
+  red_channel = image[:, :, 0]
+  green_channel = image[:, :, 1]
+  blue_channel = image[:, :, 2]
+  return red_channel, green_channel, blue_channel
+''', language="python")
+red_image, green_image, blue_image = get_RGB_channels(image)
+original_column, red_column, green_column, blue_column = st.columns(4)
+with original_column:
+  st.image(image, caption="Original Image", use_container_width=True)
+with red_column:
+  st.image(red_image, caption="Red Channel", use_container_width=True)
+with green_column:
+  st.image(green_image, caption="Green Channel", use_container_width=True)
+with blue_column:
+  st.image(blue_image, caption="Blue Channel", use_container_width=True)
